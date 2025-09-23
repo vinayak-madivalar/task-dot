80b61b3d322bed2d08c066dbcba8f47284053cdf
vinayak-madivalar <vinayak.m0157@gmail.com>
2025-09-23 21:14:31 +0530

    feat: Setup a Node.js server and database connection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// basic config
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// cors config
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Home page of Task Management app");
--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import dbConnection from "./db/dbConnection.js";
+
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`backend listening on port http://localhost:${port}`);
-});
+dbConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`backend listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+    process.exit(1);
+  });
